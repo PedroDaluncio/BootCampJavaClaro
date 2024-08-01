215020330e3e48084358bdadc91ea9f41a4bf612
Pedro <pedrodalsenter@hotmail.com>
2024-08-01 19:19:18 -0300

    started learning the inicial sintax for java

---
+++ sintaxeBasica/src/BasicSintax.java
@@ -0,0 +1,8 @@
+public class BasicSintax {
+    public static void main (String [] args){
+        System.out.print(somar(500, 270));
+    }
+    public static int somar(int valor1, int valor2){
+        return valor1 + valor2;
+    }
+}
